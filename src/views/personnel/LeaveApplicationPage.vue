b6f37796cdafb30544ba262e39200e62030015fa
johnernest11 <johnernest.catungal1997@gmail.com>
2025-06-03 16:15:38 +0800

    s

--- src/views/personnel/LeaveApplicationPage.vue
+++ src/views/personnel/LeaveApplicationPage.vue
@@ -270,20 +270,20 @@ const formatDate = (dateString: string | null | undefined): string => {
                     <template v-if="props.data.status === 'Draft'">
                       <Chip
                         label="Draft"
-                        class="flex items-center justify-center bg-error-600 px-4 py-1 font-semibold text-clear-900"
+                        class="flex items-center justify-center bg-error-800 px-4 py-1 font-semibold text-clear-500"
                       >
                       </Chip>
                     </template>
                     <template v-else-if="props.data.status === 'Review'">
                       <Chip
                         label="For Review"
-                        class="flex items-center justify-center bg-success-600 px-4 py-1 font-semibold text-clear-900"
+                        class="flex items-center justify-center bg-success-800 px-4 py-1 font-semibold text-clear-500"
                       />
                     </template>
                     <template v-else-if="props.data.status === 'Approved'">
                       <Chip
                         label="Approved"
-                        class="flex items-center justify-center bg-info-600 px-4 py-1 font-semibold text-clear-900"
+                        class="flex items-center justify-center bg-info-800 px-4 py-1 font-semibold text-clear-500"
                       />
                     </template>
                   </template>
