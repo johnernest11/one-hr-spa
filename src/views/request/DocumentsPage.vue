0eb8eef808028eb457d6797e4609ef3a6027b535
johnernest11 <johnernest.catungal1997@gmail.com>
2025-06-05 14:14:42 +0800

    s

--- src/views/request/DocumentsPage.vue
+++ src/views/request/DocumentsPage.vue
@@ -183,129 +183,138 @@ const exportPdf = async (documentRequest: DocumentRequestResponse) => {
           </div>
         </div>
       </div>
-      <div class="mx-auto flex h-full w-full flex-col">
-        <DataTable :value="documentRequestStore.documentRequest" class="mt-6" dataKey="id">
-          <Column
-            field="period"
-            header="Leave Period"
-            headerClass="w-1/2 bg-surface-100 border-surface-300 opacity-70 font-bold py-2"
-          >
-            <template #body="props">
-              <p class="font-semibold uppercase text-surface-600">{{ props.data.certificate_type }}</p>
-            </template>
-          </Column>
-          <Column
-            field="edited_at"
-            header="Date Filed"
-            headerClass=" w-64 bg-surface-100 border-surface-300 opacity-70 font-bold py-2"
-          >
-            <template #body="props">
-              <p class="uppercase text-surface-600">{{ formatDate(props.data.request_date) }}</p>
-            </template>
-          </Column>
-          <Column field="status" header="Status" headerClass="w-64 bg-surface-100 border-surface-300 opacity-70 font-bold py-2">
-            <template #body="props">
-              <template v-if="props.data.status === 'Draft'">
-                <Chip
-                  label="Draft"
-                  class="flex items-center justify-center !bg-surface-500 px-4 py-1 font-semibold !text-surface-0"
+      <div class="mt-6 flex flex-col">
+        <div class="w-full">
+          <div class="mx-auto flex h-full w-full flex-col">
+            <DataTable :value="documentRequestStore.documentRequest" class="mt-6" dataKey="id">
+              <Column
+                field="period"
+                header="Leave Period"
+                headerClass="w-1/2 bg-surface-100 border-surface-300 opacity-70 font-bold py-2"
+              >
+                <template #body="props">
+                  <p class="font-semibold uppercase text-surface-600">{{ props.data.certificate_type }}</p>
+                </template>
+              </Column>
+              <Column
+                field="edited_at"
+                header="Date Filed"
+                headerClass=" w-64 bg-surface-100 border-surface-300 opacity-70 font-bold py-2"
+              >
+                <template #body="props">
+                  <p class="uppercase text-surface-600">{{ formatDate(props.data.request_date) }}</p>
+                </template>
+              </Column>
+              <Column
+                field="status"
+                header="Status"
+                headerClass="w-64 bg-surface-100 border-surface-300 opacity-70 font-bold py-2"
+              >
+                <template #body="props">
+                  <template v-if="props.data.status === 'Draft'">
+                    <Chip
+                      label="Draft"
+                      class="flex items-center justify-center !bg-surface-500 px-4 py-1 font-semibold !text-surface-0"
+                    >
+                    </Chip>
+                  </template>
+                  <template v-else-if="props.data.status === 'Disapproved'">
+                    <Chip
+                      label="Disapproved"
+                      class="flex items-center justify-center !bg-error-800 px-4 py-1 font-semibold !text-surface-0"
+                    />
+                  </template>
+                  <template v-else-if="props.data.status === 'Approved'">
+                    <Chip
+                      label="Approved"
+                      class="flex items-center justify-center !bg-info-800 px-4 py-1 font-semibold !text-surface-0"
+                    />
+                  </template>
+                </template>
+              </Column>
+              <Column field="action" header="Actions" headerClass="w-64 bg-surface-100 opacity-70 font-bold py-2">
+                <template #body="props">
+                  <div class="flex gap-4 whitespace-nowrap md:w-auto">
+                    <Button
+                      icon="pi pi-eye"
+                      v-tooltip.top="'View Leave Application'"
+                      severity="info"
+                      class="border-none text-lg font-semibold text-primary-600 dark:text-primary-100 sm:text-primary-400 md:text-primary-500 lg:text-primary-500 dark:lg:text-primary-500"
+                      text
+                      @click="navigateToDetails(props.data)"
+                    />
+                    <Button
+                      icon="pi pi-file-pdf"
+                      v-tooltip.top="'View Leave Application'"
+                      severity="info"
+                      class="border-none text-lg font-semibold text-primary-600 dark:text-primary-100 sm:text-primary-400 md:text-primary-500 lg:text-primary-500 dark:lg:text-primary-500"
+                      text
+                      @click="exportPdf(props.data)"
+                    />
+                  </div>
+                </template>
+              </Column>
+            </DataTable>
+          </div>
+          <div class="mt-6 flex w-full justify-center md:mt-10">
+            <Paginator
+              v-if="pagination && pagination.total > 0"
+              :first="(pagination.current_page - 1) * pagination.per_page"
+              :rows="pagination.per_page"
+              :total-records="pagination.total"
+              template="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
+              currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+              @page="(event: PageState) => handlePaginationPageChange(event)"
+              class="text-s md:text-sm"
+              :pt="{ pageButton: {} }"
+            />
+          </div>
+        </div>
+        <div
+          v-if="searchSubmitted && !documentRequestIsLoading && !documentRequestStore.documentRequest.length"
+          class="flex h-full w-full flex-col items-center justify-center font-menu text-lg dark:text-surface-300"
+        >
+          <i class="pi pi-exclamation-triangle mb-2 text-2xl"></i>
+          <p>No Leave Applications found</p>
+        </div>
+        <div
+          v-if="!documentRequestIsLoading && !documentRequestStore.documentRequest.length && !searchSubmitted"
+          class="mx-auto flex h-full w-full flex-col"
+        >
+          <Card class="w-full p-0 shadow-none">
+            <template #content>
+              <div class="flex flex-col items-center">
+                <div
+                  class="my-6 flex w-full flex-col items-center justify-between gap-4 rounded-lg bg-surface-0 px-6 py-6 dark:bg-surface-800 md:my-4 md:flex-row md:px-4 md:py-4"
+                ></div>
+                <div class="flex justify-center">
+                  <img src="@/assets/image/undraw_filing-system.svg" class="w-96 pt-36" />
+                </div>
+                <h2
+                  class="mb-2 mt-4 flex w-full justify-center text-center text-xl font-semibold text-surface-800 dark:text-primary-100 sm:text-2xl"
                 >
-                </Chip>
-              </template>
-              <template v-else-if="props.data.status === 'Disapproved'">
-                <Chip
-                  label="Disapproved"
-                  class="flex items-center justify-center !bg-error-800 px-4 py-1 font-semibold !text-surface-0"
-                />
-              </template>
-              <template v-else-if="props.data.status === 'Approved'">
-                <Chip
-                  label="Approved"
-                  class="flex items-center justify-center !bg-info-800 px-4 py-1 font-semibold !text-surface-0"
-                />
-              </template>
-            </template>
-          </Column>
-          <Column field="action" header="Actions" headerClass="w-64 bg-surface-100 opacity-70 font-bold py-2">
-            <template #body="props">
-              <div class="flex gap-4 whitespace-nowrap md:w-auto">
-                <Button
-                  icon="pi pi-eye"
-                  v-tooltip.top="'View Leave Application'"
-                  severity="info"
-                  class="border-none text-lg font-semibold text-primary-600 dark:text-primary-100 sm:text-primary-400 md:text-primary-500 lg:text-primary-500 dark:lg:text-primary-500"
-                  text
-                  @click="navigateToDetails(props.data)"
-                />
-                <Button
-                  icon="pi pi-file-pdf"
-                  v-tooltip.top="'View Leave Application'"
-                  severity="info"
-                  class="border-none text-lg font-semibold text-primary-600 dark:text-primary-100 sm:text-primary-400 md:text-primary-500 lg:text-primary-500 dark:lg:text-primary-500"
-                  text
-                  @click="exportPdf(props.data)"
-                />
+                  You have no Request Documents
+                </h2>
+                <h1 class="mb-4 text-center text-base text-surface-600 dark:text-surface-400 sm:text-lg">
+                  Requested Documents created by you shall appear here.
+                </h1>
+                <div class="mt-4 flex w-full justify-center">
+                  <RouterLink :to="{ name: 'my-leaveapplications/store' }">
+                    <Button
+                      icon="pi pi-plus"
+                      label="Request Documents"
+                      severity="info"
+                      size="large"
+                      class="border border-primary-400 text-lg font-semibold text-primary-400 dark:text-primary-100 sm:text-primary-400 md:text-primary-400 lg:text-primary-400 dark:lg:text-primary-400"
+                      text
+                    />
+                  </RouterLink>
+                </div>
               </div>
             </template>
-          </Column>
-        </DataTable>
+          </Card>
+        </div>
       </div>
-      <div class="mt-6 flex w-full justify-center md:mt-10">
-        <Paginator
-          v-if="pagination && pagination.total > 0"
-          :rows="pagination.per_page"
-          :total-records="pagination.total"
-          template="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink"
-          currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
-          @page="(event: PageState) => handlePaginationPageChange(event)"
-          class="text-s md:text-sm"
-          :pt="{ pageButton: {} }"
-        />
-      </div>
-    </div>
-    <div
-      v-if="searchSubmitted && !documentRequestIsLoading && !documentRequestStore.documentRequest.length"
-      class="flex h-full w-full flex-col items-center justify-center font-menu text-lg dark:text-surface-300"
-    >
-      <i class="pi pi-exclamation-triangle mb-2 text-2xl"></i>
-      <p>No Leave Applications found</p>
-    </div>
-    <div
-      v-if="!documentRequestIsLoading && !documentRequestStore.documentRequest.length && !searchSubmitted"
-      class="mx-auto flex h-full w-full flex-col"
-    >
-      <Card class="w-full p-0 shadow-none">
-        <template #content>
-          <div class="flex flex-col items-center">
-            <div
-              class="my-6 flex w-full flex-col items-center justify-between gap-4 rounded-lg bg-surface-0 px-6 py-6 dark:bg-surface-800 md:my-4 md:flex-row md:px-4 md:py-4"
-            ></div>
-            <div class="flex justify-center">
-              <img src="@/assets/image/undraw_filing-system.svg" class="w-96 pt-36" />
-            </div>
-            <h2
-              class="mb-2 mt-4 flex w-full justify-center text-center text-xl font-semibold text-surface-800 dark:text-primary-100 sm:text-2xl"
-            >
-              You have no Request Documents
-            </h2>
-            <h1 class="mb-4 text-center text-base text-surface-600 dark:text-surface-400 sm:text-lg">
-              Requested Documents created by you shall appear here.
-            </h1>
-            <div class="mt-4 flex w-full justify-center">
-              <RouterLink :to="{ name: 'my-leaveapplications/store' }">
-                <Button
-                  icon="pi pi-plus"
-                  label="Request Documents"
-                  severity="info"
-                  size="large"
-                  class="border border-primary-400 text-lg font-semibold text-primary-400 dark:text-primary-100 sm:text-primary-400 md:text-primary-400 lg:text-primary-400 dark:lg:text-primary-400"
-                  text
-                />
-              </RouterLink>
-            </div>
-          </div>
-        </template>
-      </Card>
     </div>
   </div>
 </template>
