5758992b9d57ae50798fba852658fd55bb8d2ecf
johnernest11 <johnernest.catungal1997@gmail.com>
2025-07-01 09:44:20 +0800

    Pipeline Issue

--- src/utils/mock-data.ts
+++ src/utils/mock-data.ts
@@ -302,7 +302,7 @@ const employee_data = [
   },
   {
     id: 6,
-    individual_basic_detail_id: individual_basic_details[5],
+    individual_basic_detail_id: individual_basic_details[4],
     id_number: 'EMP-2025-006',
     item_id: item_id,
     salary_grade_id: mockSalaryGrade,
