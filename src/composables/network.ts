cc2c7f25326c306b6519dd1c6f989e2adcd72adc
Marianne Cyra Jun R. Quinzon <mcjrquinzon@ENTDSWD.LOCAL>
2025-10-27 11:21:51 +0800

    Added Access-Control-Allow-Origin to headers

--- src/composables/network.ts
+++ src/composables/network.ts
@@ -28,6 +28,7 @@ export const useApiCall = (uri: string, authToken: string | null = null) => {
       options.headers = {
         ...options.headers,
         Authorization: `Bearer ${authToken}`,
+        'Access-Control-Allow-Origin': '*',
       }
 
       return { options, url }
